a589c2f3314c481a6066dd7e3f5bc0532a363981
todstoychev <todstoychev@gmail.com>
2016-02-16 20:20:42 +0200

    Docblock added

--- src/Processor.php
+++ src/Processor.php
@@ -35,6 +35,9 @@ class Processor
      */
     protected $openImageHandler;
 
+    /**
+     * @param array $config
+     */
     public function __construct(array $config)
     {
         $this->config = $config;
